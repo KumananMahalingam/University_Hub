daf731f794d631ef2c1a850a75d4ec06085636e6
KumananMahalingam <mkumanan@outlook.com>
2026-05-24 17:06:37 -0400

    Reconfigured major logic

---
+++ backend/src/model/Major.java
@@ -0,0 +1,21 @@
+package com.universityhub.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "majors")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Major {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+}
