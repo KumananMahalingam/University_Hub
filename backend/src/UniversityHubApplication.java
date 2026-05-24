8139d5e99f69b126626dfdf0e2e817cdf3fe16c4
KumananMahalingam <mkumanan@outlook.com>
2026-05-24 16:54:07 -0400

    springboot backend configured

---
+++ backend/src/UniversityHubApplication.java
@@ -0,0 +1,11 @@
+package com.universityhub;
+ 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication
+public class UniversityHubApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityHubApplication.class, args);
+    }
+}
